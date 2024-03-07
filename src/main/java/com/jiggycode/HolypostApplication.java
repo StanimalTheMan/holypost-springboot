f49c58adf44c484af1a29b12c538bac6539ef3f0
Stan Choi <sjc591@nyu.edu>
2024-03-07 12:48:46 -0500

    Initialize springboot project

---
+++ src/main/java/com/jiggycode/HolypostApplication.java
@@ -0,0 +1,13 @@
+package com.jiggycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolypostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolypostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jiggycode/HolypostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiggycode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolypostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
